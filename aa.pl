0330d8f5ad0007603ac289e08efa2e9ffcf5fb2e
Andrei Lapets <a@lapets.io>
2009-12-30 13:07:50 -0500

    Add Perl wrapper script for command line.

---
+++ aa.pl
@@ -0,0 +1,24 @@
+################################################################
+##
+## aartifact
+## http://www.aartifact.org/src/
+## Copyright (C) 2008-2010
+## A. Lapets
+##
+## This software is made available under the GNU GPLv3.
+##
+## aa.pl
+##   Perl wrapper script that can be useful if ANSI color codes
+##   are not displayed properly by the console or environment.
+
+################################################################
+##
+
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my $args = "";
+foreach my $i (0 .. $#ARGV) {$args .= "$ARGV[$i] ";}
+my @o = qx(aa $args);
+foreach my $l (@o) { print $l; }
